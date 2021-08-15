cf3a7857d90e2bdbf6ebcaec75de9058a346e5fd
Trevor-Davis <56545070+Trevor-Davis@users.noreply.github.com>
2021-08-15 11:51:38 -0400

    commit

--- HCX Port Check/hcxportcheck.ps1
+++ HCX Port Check/hcxportcheck.ps1
@@ -2,7 +2,8 @@ $vcenterip = Read-Host -Prompt "
 IP Address of AVS vCenter: "
 $nsxip = Read-Host -Prompt "
 IP Address of AVS NSX Manager: "
-$hcxip = Read-Host -Prompt "
+$hcxip = Read-Host -Prompt "  
 IP Address of AVS HCX Manager: "
 
 $vcenterporttest = Test-NetConnection -ComputerName $vcenterip -Port 445
+
