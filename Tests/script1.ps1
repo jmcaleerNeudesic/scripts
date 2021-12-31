9101c3bcd37b2232791b6b24d923be67fafcfbf4
Trevor-Davis <56545070+Trevor-Davis@users.noreply.github.com>
2021-12-31 18:56:30 +0000

    Add files via upload

---
+++ Tests/script1.ps1
@@ -0,0 +1,9 @@
+Write-Host "hello"
+& "c:\temp\script2.ps1"
+Write-Host "3rd"
+Write-Host -NoNewline -ForegroundColor White "white"
+Write-Host -ForegroundColor Green " green"
+
+Write-Host -NoNewLine -ForegroundColor Green "
+Provide the credentials for the"
+Write-Host -ForegroundColor White " AVS NSX Manager:"
