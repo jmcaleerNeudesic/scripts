268199459d3fed3dd47b3dad6cfbde8e2b09d86d
Trevor-Davis <56545070+Trevor-Davis@users.noreply.github.com>
2022-02-07 23:21:39 -0500

    Update script1.ps1

--- Tests/script1.ps1
+++ Tests/script1.ps1
@@ -1,8 +1,2 @@
-Write-Host "hello"
-Write-Host "3rd"
-Write-Host -NoNewline -ForegroundColor White "white"
-Write-Host -ForegroundColor Green " green"
-
-Write-Host -NoNewLine -ForegroundColor Green "
-Provide the credentials for the"
-Write-Host -ForegroundColor White " AVS NSX Manager:"
+$trevor = "my name is trevor"
+write-host $trevor
