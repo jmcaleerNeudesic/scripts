5865717c3b203dc0e1c1ef039e10beeab68bb9e4
Trevor-Davis <56545070+Trevor-Davis@users.noreply.github.com>
2022-02-09 01:33:03 +0000

    Add files via upload

--- AVS Private Cloud Deployment/ConnectToAzure.ps1
+++ AVS Private Cloud Deployment/ConnectToAzure.ps1
@@ -1,5 +1,8 @@
 $sub = "3988f2d0-8066-42fa-84f2-5d72f80901da"
 
-write-host -ForegroundColor Green "
+write-host -ForegroundColor Blue "
 Connecting to your Azure Subscription $sub ... there should be a web browser pop-up ... go there to login"
 Connect-AzAccount -Subscription $sub 
+write-host -ForegroundColor Green "
+Azure Login Successful
+"
---
+++ AVS Private Cloud Deployment/validatesubready.ps1
@@ -0,0 +1,36 @@
+$regionfordeployment = "Southeast Asia"
+
+
+Write-Host -ForegroundColor Yellow  "
+Validating Subscription Readiness ..." 
+
+$quota = Test-AzVMWareLocationQuotaAvailability -Location $regionfordeployment
+
+if ("Enabled" -eq $quota.Enabled)
+{
+
+Write-Host -ForegroundColor Green "
+Success: Quota is Enabled on Subscription
+"    
+
+Register-AzResourceProvider -ProviderNamespace Microsoft.AVS
+
+Write-Host -ForegroundColor Green "
+Success: Resource Provider Enabled
+"    
+
+Start-Sleep 5
+}
+
+
+Else
+
+{
+Write-Host -ForegroundColor Red "
+Subscription $sub Does NOT Have Quota for Azure VMware Solution, please visit the following site for guidance on how to get this service enabled for your subscription.
+
+https://docs.microsoft.com/en-us/azure/azure-vmware/enable-azure-vmware-solution"
+
+Exit
+
+}
