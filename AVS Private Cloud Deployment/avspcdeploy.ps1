448132475fa4d300f594e20585cacc45e938f62c
Trevor-Davis <56545070+Trevor-Davis@users.noreply.github.com>
2022-02-08 20:34:12 -0500

    Update avspcdeploy.ps1

--- AVS Private Cloud Deployment/avspcdeploy.ps1
+++ AVS Private Cloud Deployment/avspcdeploy.ps1
@@ -1,2 +1,4 @@
 $ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/Trevor-Davis/scripts/main/AVS%20Private%20Cloud%20Deployment/ConnectToAzure.ps1
 Invoke-Expression $($ScriptFromGitHub.Content)
+$ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/Trevor-Davis/scripts/main/AVS%20Private%20Cloud%20Deployment/validatesubready.ps1
+Invoke-Expression $($ScriptFromGitHub.Content)
