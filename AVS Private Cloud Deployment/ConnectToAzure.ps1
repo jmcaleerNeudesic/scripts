fb68fee39ad7f5f9960655dba89c1382b0d39763
Trevor-Davis <56545070+Trevor-Davis@users.noreply.github.com>
2022-02-09 01:42:12 +0000

    Add files via upload

--- AVS Private Cloud Deployment/ConnectToAzure.ps1
+++ AVS Private Cloud Deployment/ConnectToAzure.ps1
@@ -1,8 +1,9 @@
 $sub = "3988f2d0-8066-42fa-84f2-5d72f80901da"
 
-write-host -ForegroundColor Blue "
+write-host -ForegroundColor Yellow "
 Connecting to your Azure Subscription $sub ... there should be a web browser pop-up ... go there to login"
-Connect-AzAccount -Subscription $sub 
+$command = Connect-AzAccount -Subscription $sub
+$command | ConvertTo-Json
 write-host -ForegroundColor Green "
 Azure Login Successful
 "
