924cf1883c393cd0ac7b099ecffb1ec73a3f76bc
Trevor-Davis <56545070+Trevor-Davis@users.noreply.github.com>
2022-02-08 21:54:42 -0500

    Create vartest.ps1

---
+++ AVS Private Cloud Deployment/vartest.ps1
@@ -0,0 +1,11 @@
+$regionfordeployment = "Southeast Asia"
+$RGNewOrExisting = "Existing"
+$sub = "1178f22f-6ce4-45e3-bd92-ba89930be5be"
+$ExrGatewayForAVS = "ExRGW-VirtualWorkloads-APAC-Hub"
+$pcname = "AVS1-VirtualWorkloads-APAC-AzureCloud"
+$rgfordeployment = "VirtualWorkloads-APAC-AzureCloud"
+$addressblock = "10.1.0.0/22"
+$skus = "AV36"
+$numberofhosts = "3"
+$internet = "Enabled"
+
