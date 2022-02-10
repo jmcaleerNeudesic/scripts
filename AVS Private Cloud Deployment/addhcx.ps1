0d6b7b38ba5395ee7d90d656be9f8efe6c4ee652
Trevor-Davis <56545070+Trevor-Davis@users.noreply.github.com>
2022-02-09 20:21:10 -0500

    Delete AVS Private Cloud Deployment directory

--- AVS Private Cloud Deployment/addhcx.ps1
+++
@@ -1,2 +0,0 @@
-# Deploy HCX Advanced
-az vmware addon hcx create --resource-group "VirtualWorkloads-APAC-AzureCloud" --private-cloud "AVS2-VirtualWorkloads-APAC-AzureCloud" --offer "VMware MaaS Cloud Provider"
