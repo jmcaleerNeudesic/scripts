e8cd518804035ae67c5d283fc21bfa866461305c
Trevor-Davis <56545070+Trevor-Davis@users.noreply.github.com>
2022-07-19 09:25:20 -0400

    updates

---
+++ myscripts.ps1
@@ -0,0 +1,12 @@
+$TTL    604800
+@       IN      SOA     ns.virtualworkloads.trevor. admin.virtualworkloads.trevor. (
+                              2        ; Serial
+                         604800        ; Refresh
+                          86400        ; Retry
+                        2419200        ; Expire
+                         604800 )      ; Negative Cache TTL
+;
+@       IN      NS      ns.virtualworkloads.trevor.
+@       IN      A       127.0.0.1
+ns      IN      A       127.0.0.1
+hcx     IN      A       137.52.224.11
