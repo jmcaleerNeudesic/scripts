1366a2949e8ee3667d297cb07a90cc3aa275acce
Trevor-Davis <56545070+Trevor-Davis@users.noreply.github.com>
2022-07-19 09:35:26 -0400

    updats

--- myscripts.ps1
+++ myscripts.ps1
@@ -17,31 +17,3 @@ $filelistarray = @()
 $filelistarray += $buildhol_ps1, $avsdeploy_ps1
 $filelistarray
 $skipvariables = $filelistarray
-
-
-
-
-
-if ($buildhol_ps1 -notmatch "Yes" -and $avsdeploy_ps1 -notmatch "Yes"
-
-$array = @("azureloginfunction.ps1", "checkavsvcentercommunicationfunction.ps1", "getfilesizefunction.ps1") 
-foreach ($filename in $array){ 
-  Write-Host "Downloading $filename"
-  Invoke-WebRequest -uri "https://raw.githubusercontent.com/Trevor-Davis/Azure-VMware-Solution/master/AVSSimplifiedDeployment/$filename" -OutFile $env:TEMP\AVSDeploy\$filename
-  . $env:TEMP\AVSDeploy\$filename
-}
-
-
-
-
-
-, "checkavsvcentercommunicationfunction.ps1", "getfilesizefunction.ps1"
-
-Write-Host "$$filelistarray"
-
-$buildhol_ps1 -notmatch "Yes" -and $avsdeploy_ps1 -notmatch "Yes"
-
-
-){
-
-
