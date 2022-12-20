b75311b9735daeb3ff55c6e8462f6f273bd42899
tredavis <trevor.davis@hotmail.com>
2022-12-20 15:22:19 -0500

    update

--- Tests/script1.ps1
+++ Tests/script1.ps1
@@ -1,4 +1,4 @@
-$filename = "Function-writeingreen.ps1"
+$filename = "Function-WriteInGreen.ps1"
 write-host "Downloading" $filename
 Invoke-WebRequest -uri "https://raw.githubusercontent.com/Trevor-Davis/AzureScripts/main/Functions/$filename" -OutFile $env:TEMP\$folder\$filename
 . $env:TEMP\$folder\$filename
